acf440196e45605080a00fc6d7968712a032fe64
aolosolo <akmal349434@gmail.com>
2025-07-06 12:57:31 +0500

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // IMPORTANT: Your Firebase project configuration is read from the .env file.
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCqu9nj7UdiFP2a39TjE0mEBBjW8LLKoiA",
+  authDomain: "fastgrab.firebaseapp.com",
+  projectId: "fastgrab",
+  storageBucket: "fastgrab.firebasestorage.app",
+  messagingSenderId: "835565121090",
+  appId: "1:835565121090:web:23f55570c915761b7687db"
 };
 
 // Initialize Firebase using a singleton pattern to avoid re-initialization in Next.js
